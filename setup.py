738562c0ff42f34983caa7c24407e412f6d26327
Anthony Scopatz <scopatz@gmail.com>
2015-07-26 11:21:02 -0500

    installation fix

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup
+#!/usr/bin/env python
 import os.path
+
+from setuptools import setup
 
 
 tl_package = 'obvci'
@@ -12,14 +14,32 @@ except IOError:
     version = 'dev'
 
 
-if __name__ == '__main__':
-    setup(name='conda-smithy',
-          version='0.1.0dev0',
-          description='A package to create repositories for conda recipes, and automate their building with CI tools on Linux, OSX and Windows.',
-          author='Phil Elson',
-          author_email='pelson.pub@gmail.com',
-          url='https://github.com/conda-forge/conda-smithy',
-          scripts=[os.path.join('scripts', 'conda-smithy')],
-          packages=['conda_smithy'],
-         )
+def main():
+    skw = dict(
+        name='conda-smithy',
+        version='0.1.0dev0',
+        description='A package to create repositories for conda recipes, and automate '
+                    'their building with CI tools on Linux, OSX and Windows.',
+        author='Phil Elson',
+        author_email='pelson.pub@gmail.com',
+        url='https://github.com/conda-forge/conda-smithy',
+        scripts=[os.path.join('scripts', 'conda-smithy')],
+        packages=['conda_smithy', 
+                  'conda_smithy.feedstock_content', 
+                  'conda_smithy.feedstock_content.ci_support',
+                  ],
+        package_dir={'conda_smithy': 'conda_smithy', 
+                     'conda_smithy.feedstock_content': 'feedstock_content', 
+                     'conda_smithy.feedstock_content.ci_support': 
+                        'feedstock_content/ci_support', 
+                     },
+        package_data={'conda_smithy.feedstock_content': ['README', '*.*'],
+                      'conda_smithy.feedstock_content.ci_support': ['*'],
+                      },
+        zip_safe=False,
+        )
+    setup(**skw)
 
+
+if __name__ == '__main__':
+    main()
