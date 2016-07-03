0151a38412d2bab49853071205a9ce0f342c5709
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-07-03 15:32:33 -0400

    cli: Sleep for 1 second between queries to Travis CI.

--- conda_smithy/cli.py
+++ conda_smithy/cli.py
@@ -4,6 +4,7 @@ import os
 import requests
 import subprocess
 import sys
+import time
 import argparse
 
 from conda_build.metadata import MetaData
@@ -156,6 +157,7 @@ class RegisterCI(Subcommand):
         try:
             ci_register.add_project_to_travis(owner, repo)
             ci_register.travis_token_update_conda_forge_config(args.feedstock_directory, owner, repo)
+            time.sleep(1)
             ci_register.travis_configure(owner, repo)
             ci_register.add_project_to_circle(owner, repo)
             ci_register.add_token_to_circle(owner, repo)
