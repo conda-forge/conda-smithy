dd55c4365e63ae66290990353db07b66a4ef5b5c
shadow_walker <shadowwalkersb@gmail.com>
2016-09-06 18:55:55 -0500

    refactor: extract common part of paths into variable

--- appveyor/setup_x64.bat
+++ appveyor/setup_x64.bat
@@ -9,5 +9,6 @@ regedit /s x64\656d8760-2429-11d7-8bf6-00b0d03daa06.reg
 regedit /s x64\656d8763-2429-11d7-8bf6-00b0d03daa06.reg
 regedit /s x64\656d8766-2429-11d7-8bf6-00b0d03daa06.reg
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.Express.config"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.Express.config"
+set "common_dir=C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\"
+copy "%common_dir%\vcpackages\\AMD64.VCPlatform.config"   "%common_dir%\vcpackages\\AMD64.VCPlatform.Express.config"
+copy "%common_dir%\vcpackages\\Itanium.VCPlatform.config" "%common_dir%\vcpackages\\Itanium.VCPlatform.Express.config"
