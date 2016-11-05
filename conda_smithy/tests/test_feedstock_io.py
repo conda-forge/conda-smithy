89c1530882c67a135687df389d0a96d2283873c8
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-11-05 18:42:04 -0400

    Add some tests for `get_repo`.

--- conda_smithy/tests/test_feedstock_io.py
+++ conda_smithy/tests/test_feedstock_io.py
@@ -6,7 +6,41 @@ import shutil
 import tempfile
 import unittest
 
+import git
+
 import conda_smithy.feedstock_io as fio
+
+
+def keep_dir(dirname):
+    keep_filename = os.path.join(dirname, ".keep")
+    with io.open(keep_filename, "w", encoding = "utf-8") as fh:
+        fh.write("")
+
+
+def parameterize():
+    for pathfunc in [
+        lambda pth, tmp_dir: os.path.relpath(pth, tmp_dir),
+        lambda pth, tmp_dir: pth
+    ]:
+        for get_repo in [
+            lambda tmp_dir: None,
+            lambda tmp_dir: git.Repo.init(tmp_dir)
+        ]:
+            try:
+                tmp_dir = tempfile.mkdtemp()
+                keep_dir(tmp_dir)
+
+                old_dir = os.getcwd()
+                os.chdir(tmp_dir)
+
+                yield (
+                    tmp_dir,
+                    get_repo(tmp_dir),
+                    lambda pth: pathfunc(pth, tmp_dir)
+                )
+            finally:
+                os.chdir(old_dir)
+                shutil.rmtree(tmp_dir)
 
 
 class TestFeedstockIO(unittest.TestCase):
@@ -18,6 +52,19 @@ class TestFeedstockIO(unittest.TestCase):
 
         with io.open(os.path.abspath(".keep"), "w", encoding="utf-8") as fh:
             fh.write("")
+
+
+    def test_repo(self):
+        for tmp_dir, repo, pathfunc in parameterize():
+            if repo is None:
+                self.assertTrue(
+                    fio.get_repo(pathfunc(tmp_dir)) is None
+                )
+            else:
+                self.assertIsInstance(
+                    fio.get_repo(pathfunc(tmp_dir)),
+                    git.Repo
+                )
 
 
     def tearDown(self):
