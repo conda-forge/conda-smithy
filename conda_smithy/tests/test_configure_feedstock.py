f92c01fed87adc7d8b028133aa211145da964307
Bruno Oliveira <nicoddemus@gmail.com>
2016-11-15 14:01:06 -0200

    Update test_configure_feedstock to pytest's style

--- conda_smithy/tests/test_configure_feedstock.py
+++ conda_smithy/tests/test_configure_feedstock.py
@@ -1,46 +1,31 @@
-from contextlib import contextmanager
-import os
-import shutil
-import tempfile
-import unittest
-
 import conda_build.metadata
 import conda.api
 
 import conda_smithy.configure_feedstock as cnfgr_fdstk
 
 
-@contextmanager
-def tmp_directory():
-    tmp_dir = tempfile.mkdtemp('_recipe')
-    yield tmp_dir
-    shutil.rmtree(tmp_dir)
-
-
-class Test_fudge_subdir(unittest.TestCase):
-    def test_metadata_reading(self):
-        with tmp_directory() as recipe_dir:
-            with open(os.path.join(recipe_dir, 'meta.yaml'), 'w') as fh:
-                fh.write("""
+class Test_fudge_subdir(object):
+    def test_metadata_reading(self, tmpdir):
+        tmpdir.join('meta.yaml').write("""
                         package:
                            name: foo_win  # [win]
                            name: foo_osx  # [osx]
                            name: foo_the_rest  # [not (win or osx)]
                          """)
-            meta = conda_build.metadata.MetaData(recipe_dir)
-            config = cnfgr_fdstk.meta_config(meta)
+        meta = conda_build.metadata.MetaData(str(tmpdir))
+        config = cnfgr_fdstk.meta_config(meta)
 
-            kwargs = {}
-            if hasattr(conda_build, 'api'):
-                kwargs['config'] = config
+        kwargs = {}
+        if hasattr(conda_build, 'api'):
+            kwargs['config'] = config
 
-            with cnfgr_fdstk.fudge_subdir('win-64', config):
-                meta.parse_again(**kwargs)
-                assert meta.name() == 'foo_win'
+        with cnfgr_fdstk.fudge_subdir('win-64', config):
+            meta.parse_again(**kwargs)
+            assert meta.name() == 'foo_win'
 
-            with cnfgr_fdstk.fudge_subdir('osx-64', config):
-                meta.parse_again(**kwargs)
-                assert meta.name() == 'foo_osx'
+        with cnfgr_fdstk.fudge_subdir('osx-64', config):
+            meta.parse_again(**kwargs)
+            assert meta.name() == 'foo_osx'
 
     def test_fetch_index(self):
         if hasattr(conda_build, 'api'):
@@ -57,8 +42,3 @@ class Test_fudge_subdir(unittest.TestCase):
                             ('The keys for the Windows and OSX index were the same.'
                              ' Subdir is not working and will result in mis-rendering '
                              '(e.g. https://github.com/SciTools/conda-build-all/issues/49).')
-
-
-
-if __name__ == '__main__':
-    unittest.main()
