d09b8e833b16c0c5b16d1681103ee5bcd247c7de
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-01-04 12:29:23 -0500

    Use absolute imports from future everywhere.

--- conda_smithy/configure_feedstock.py
+++ conda_smithy/configure_feedstock.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import OrderedDict as odict
 from contextlib import contextmanager
--- conda_smithy/tests/test_lint_recipe.py
+++ conda_smithy/tests/test_lint_recipe.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, print_function, unicode_literals
 from collections import OrderedDict
 from contextlib import contextmanager
 import io
