fe51ea637341d1315289d21c038bc9c34e578dbc
Isuru Fernando <isuruf@gmail.com>
2018-02-10 16:03:32 -0600

    Don't generate content in init

--- conda_smithy/cli.py
+++ conda_smithy/cli.py
@@ -19,23 +19,10 @@ from . import __version__
 
 PY2 = sys.version_info[0] == 2
 
-
-def _absolute_variant_config_paths(variant_config_files, cwd):
-    config_files = []
-    for f in ensure_list(variant_config_files):
-        if f and not os.path.isabs(f):
-            config_files.append(os.path.normpath(os.path.join(cwd, f)))
-        else:
-            config_files.append(os.path.normpath(f))
-    return config_files
-
-
-def generate_feedstock_content(target_directory, source_recipe_dir, meta, variant_config_files):
+def generate_feedstock_content(target_directory, source_recipe_dir):
     target_directory = os.path.abspath(target_directory)
     recipe_dir = "recipe"
     target_recipe_dir = os.path.join(target_directory, recipe_dir)
-
-    variant_config_files = _absolute_variant_config_paths(variant_config_files, cwd=os.getcwd())
 
     if not os.path.exists(target_recipe_dir):
         os.makedirs(target_recipe_dir)
@@ -53,14 +40,6 @@ def generate_feedstock_content(target_directory, source_recipe_dir, meta, varian
     if not os.path.exists(forge_yml):
         with feedstock_io.write_file(forge_yml) as fh:
             fh.write(u"[]")
-
-    cwd = os.getcwd()
-    variant_config_files = _absolute_variant_config_paths(variant_config_files, cwd)
-    os.chdir(target_directory)
-    try:
-        configure_feedstock.main(target_directory, variant_config_files)
-    finally:
-        os.chdir(cwd)
 
 
 class Subcommand(object):
@@ -96,8 +75,6 @@ class Init(Subcommand):
                          "created. (Default: './<packagename>-feedstock')")
         scp.add_argument("--no-git-repo", action='store_true', default=False,
                          help="Do not init the feedstock as a git repository.")
-        scp.add_argument("-m", "--variant-config-files", action="append",
-                         help="path to conda_build_config.yaml defining your base matrix")
 
     def __call__(self, args):
         # check some error conditions
@@ -119,8 +96,7 @@ class Init(Subcommand):
             os.makedirs(feedstock_directory)
             if not args.no_git_repo:
                 subprocess.check_call(['git', 'init'], cwd=feedstock_directory)
-            generate_feedstock_content(feedstock_directory, args.recipe_directory, meta,
-                                       args.variant_config_files)
+            generate_feedstock_content(feedstock_directory, args.recipe_directory)
             if not args.no_git_repo:
                 subprocess.check_call(['git', 'commit', '-m', msg], cwd=feedstock_directory)
 
