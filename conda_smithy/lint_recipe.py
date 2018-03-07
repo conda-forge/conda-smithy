6c662e824c6facf63a9439f39757f360120562fa
Patrick Sodré <sodre@sodre.co>
2018-03-06 21:10:23 -0500

    Add load_file_regex to jinja2 context

--- conda_smithy/lint_recipe.py
+++ conda_smithy/lint_recipe.py
@@ -13,7 +13,11 @@ import ruamel.yaml
 
 from conda_build.metadata import (ensure_valid_license_family,
                                   FIELDS as cbfields)
+from conda_build.jinja_context import load_file_regex
 import conda_build.conda_interface
+
+from collections import namedtuple
+
 import copy
 
 FIELDS = copy.deepcopy(cbfields)
@@ -341,10 +345,14 @@ def main(recipe_dir, conda_forge=False):
     # stub out cb3 jinja2 functions - they are not important for linting
     #    if we don't stub them out, the ruamel.yaml load fails to interpret them
     #    we can't just use conda-build's api.render functionality, because it would apply selectors
+    CondaBuildConfig = namedtuple('CondaBuildConfig', 'work_dir')
     env.globals.update(dict(compiler=lambda x: x + '_compiler_stub',
                             pin_subpackage=lambda *args, **kwargs: 'subpackage_stub',
                             pin_compatible=lambda *args, **kwargs: 'compatible_pin_stub',
                             cdt=lambda *args, **kwargs: 'cdt_stub',
+                            load_file_regex=lambda *args, **kwargs: \
+                                    load_file_regex(CondaBuildConfig(recipe_dir), \
+                                                    *args, **kwargs),
                             ))
 
     with io.open(recipe_meta, 'rt') as fh:
