f47a53948f36b032bba4041261a2643117142d10
christopher <cjwright4242@gmail.com>
2018-08-18 15:16:55 -0400

    check that the sequence isn't a string

--- conda_smithy/lint_recipe.py
+++ conda_smithy/lint_recipe.py
@@ -132,7 +132,9 @@ def lintify(meta, recipe_dir=None, conda_forge=False):
                      'the `extra/recipe-maintainers` section.')
 
     # 3b: Maintainers should be a list
-    if not isinstance(extra_section.get('recipe-maintainers', []), Sequence):
+    if not (isinstance(extra_section.get('recipe-maintainers', []), Sequence)
+            and not isinstance(extra_section.get('recipe-maintainers', []),
+                               str)):
         lints.append('Recipe maintainers should be a json list.')
 
     # 4: The recipe should have some tests.
