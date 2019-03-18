9cb4bde6e8b371d2aaa36eb99dac06161afab929
Leopold Talirz <leopold.talirz@gmail.com>
2019-03-18 15:44:22 -0400

    Update conda_smithy/lint_recipe.py
    Co-Authored-By: scopatz <scopatz@gmail.com>

--- conda_smithy/lint_recipe.py
+++ conda_smithy/lint_recipe.py
@@ -300,7 +300,7 @@ def lintify(meta, recipe_dir=None, conda_forge=False):
         lints.append(
             "Using pinned numpy packages is a deprecated pattern.  Consider "
             "using the method outlined "
-            "[here](http://conda-forge.org/docs/maintainer/knowledge_base.html#linking-numpy)."
+            "[here](https://conda-forge.org/docs/maintainer/knowledge_base.html#linking-numpy)."
         )
 
     # 16: Subheaders should be in the allowed subheadings
