1c5211f171016dd61af7a6d086b407b1e41fe291
Matthew R. Becker <becker.mr@gmail.com>
2020-03-14 14:31:45 -0500

    put in the rest of the output operations

--- conda_smithy/feedstock_outputs.py
+++ conda_smithy/feedstock_outputs.py
@@ -7,7 +7,8 @@ import json
 import hmac
 import urllib.parse
 
-import requests
+from binstar_client.utils import get_server_api
+from binstar_client import BinstarError
 
 import git
 
@@ -15,12 +16,82 @@ from .feedstock_tokens import is_valid_feedstock_token
 
 
 def copy_feedstock_outputs(outputs, staging_conda_channel, production_conda_channel):
-    pass
+    """Copy outputs from one chanel to another.
+
+    Parameters
+    ----------
+    outputs : dict
+        A dictionary mapping each full qualified output in the conda index
+        to a hash ("md5"), its name ("name"), and version ("version").
+    staging_conda_channel : str
+        The channel that currently holds the output.
+    production_conda_channel : str
+        The channel to which to copy the output.
+
+    Returns
+    -------
+    copied : dict
+        A dict keyed on the output name with True if the copy worked and False
+        otherwise.
+    """
+    from .ci_register import anaconda_token
+    ac = get_server_api(token=anaconda_token)
+
+    copied = {o: False for o in outputs}
+
+    for out_name, out in outputs.items():
+        try:
+            ac.copy(
+                staging_conda_channel,
+                out["name"],
+                out["version"],
+                basename=urllib.parse.quote(out_name, safe=""),
+                to_owner=production_conda_channel,
+                from_label='main',
+                to_label='main',
+            )
+            copied[out_name] = True
+        except BinstarError:
+            pass
+    return copied
 
 
 def validate_feedstock_outputs(
     user, project, outputs, feedstock_token, conda_channel, output_repo, token_repo, register=True
 ):
+    """Validate feedstock outputs.
+
+    Parameters
+    ----------
+    user : str
+        The GitHub user or org.
+    project : str
+        The name of the feedstock.
+    outputs : dict
+        A dictionary mapping each full qualified output in the conda index
+        to a hash ("md5"), its name ("name"), and version ("version").
+    feedstock_token : str
+        The secret token used to validate that this feedstock is who it says
+        it is.
+    conda_channel : str
+        The current conda channel with the outputs. This parameter is used to check the
+        md5 checksums from the feedstock against the ones on the channel.
+    output_repo : str
+        The GitHub repo with the feedstock output registry. This parameter is used to validate
+        that the feedstock is allowed to push to a given set of outputs.
+    token_repo : str
+        The GitHub repo with the hashed secret tokens. This repo is used as the
+        source of truth for the feedstock tokens.
+    register : bool
+        If True, attempt to register any outputs that do not exist by pushing
+        the proper json blob to `output_repo`. Default is True.
+
+    Returns
+    -------
+    valid : dict
+        A dict keyed on the keys in `outputs` with values True in the output
+        is valid and False otherwise.
+    """
     valid = {o: False for o in outputs}
 
     if not is_valid_feedstock_token(user, project, feedstock_token, token_repo):
@@ -64,24 +135,22 @@ def is_valid_output_hash(conda_channel, outputs):
         A dict keyed on output name with True if it is valid and False
         otherwise.
     """
+    # from .ci_register import anaconda_token
+    ac = get_server_api()
 
     valid = {o: False for o in outputs}
 
     for out_name, out in outputs.items():
-        url = (
-            "https://api.anaconda.org/dist/{conda_channel}/"
-            "{name}/{version}/{encoded_name}"
-        ).format(
-            conda_channel=conda_channel,
-            name=out["name"],
-            version=out["version"],
-            encoded_name=urllib.parse.quote(out_name, safe=""),
-        )
-        r = requests.get(url, headers={"Accept": "application/json"})
-        if r.status_code != 200:
-            continue
-
-        valid[out_name] = hmac.compare_digest(r.json()["md5"], out["md5"])
+        try:
+            data = ac.distribution(
+                conda_channel,
+                out["name"],
+                out["version"],
+                basename=urllib.parse.quote(out_name, safe=""),
+            )
+            valid[out_name] = hmac.compare_digest(data["md5"], out["md5"])
+        except BinstarError:
+            pass
 
     return valid
 
