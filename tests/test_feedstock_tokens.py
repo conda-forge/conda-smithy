ac4eea24f9076d0610d39fc85d54b0a0b6deb85e
Matthew R. Becker <becker.mr@gmail.com>
2020-03-14 07:54:37 -0500

    blacken

--- tests/test_feedstock_tokens.py
+++ tests/test_feedstock_tokens.py
@@ -14,8 +14,8 @@ from conda_smithy.feedstock_tokens import (
 
 
 def test_generate_and_write_feedstock_token():
-    user = 'bar'
-    repo = 'foo'
+    user = "bar"
+    repo = "foo"
 
     pth = os.path.expanduser("~/.conda-smithy/bar_foo_feedstock.token")
 
@@ -33,8 +33,8 @@ def test_generate_and_write_feedstock_token():
 
 
 def test_read_feedstock_token():
-    user = 'bar'
-    repo = 'foo'
+    user = "bar"
+    repo = "foo"
     pth = os.path.expanduser("~/.conda-smithy/bar_foo_feedstock.token")
 
     # no token
@@ -64,26 +64,21 @@ def test_read_feedstock_token():
             os.remove(pth)
 
 
-@pytest.mark.parametrize("retval", [
-    True,
-    False,
-])
-@pytest.mark.parametrize("project", [
-    "bar",
-    "bar-feedstock",
-])
-@pytest.mark.parametrize("repo", [
-    "GITHUB_TOKEN",
-    "${GITHUB_TOKEN}",
-    "GH_TOKEN",
-    "${GH_TOKEN}",
-])
+@pytest.mark.parametrize("retval", [True, False,])
+@pytest.mark.parametrize("project", ["bar", "bar-feedstock",])
+@pytest.mark.parametrize(
+    "repo", ["GITHUB_TOKEN", "${GITHUB_TOKEN}", "GH_TOKEN", "${GH_TOKEN}",]
+)
 @mock.patch("conda_smithy.feedstock_tokens.tempfile")
 @mock.patch("conda_smithy.feedstock_tokens.git")
 @mock.patch("conda_smithy.github.gh_token")
-def test_feedstock_token_exists(gh_mock, git_mock, tmp_mock, tmpdir, repo, project, retval):
+def test_feedstock_token_exists(
+    gh_mock, git_mock, tmp_mock, tmpdir, repo, project, retval
+):
     gh_mock.return_value = "abc123"
-    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(tmpdir)
+    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(
+        tmpdir
+    )
 
     user = "foo"
     os.makedirs(os.path.join(tmpdir, "tokens"), exist_ok=True)
@@ -94,28 +89,24 @@ def test_feedstock_token_exists(gh_mock, git_mock, tmp_mock, tmpdir, repo, proje
     assert feedstock_token_exists(user, project, repo) is retval
 
     assert git_mock.Repo.clone_from.called_with(
-        "abc123",
-        str(tmpdir),
-        depth=1,
+        "abc123", str(tmpdir), depth=1,
     )
 
 
-@pytest.mark.parametrize("project", [
-    "bar",
-    "bar-feedstock",
-])
-@pytest.mark.parametrize("repo", [
-    "GITHUB_TOKEN",
-    "${GITHUB_TOKEN}",
-    "GH_TOKEN",
-    "${GH_TOKEN}",
-])
+@pytest.mark.parametrize("project", ["bar", "bar-feedstock",])
+@pytest.mark.parametrize(
+    "repo", ["GITHUB_TOKEN", "${GITHUB_TOKEN}", "GH_TOKEN", "${GH_TOKEN}",]
+)
 @mock.patch("conda_smithy.feedstock_tokens.tempfile")
 @mock.patch("conda_smithy.feedstock_tokens.git")
 @mock.patch("conda_smithy.github.gh_token")
-def test_feedstock_token_raises(gh_mock, git_mock, tmp_mock, tmpdir, repo, project):
+def test_feedstock_token_raises(
+    gh_mock, git_mock, tmp_mock, tmpdir, repo, project
+):
     gh_mock.return_value = "abc123"
-    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(tmpdir)
+    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(
+        tmpdir
+    )
 
     git_mock.Repo.clone_from.side_effect = ValueError("blarg")
     user = "foo"
@@ -129,18 +120,13 @@ def test_feedstock_token_raises(gh_mock, git_mock, tmp_mock, tmpdir, repo, proje
     assert "Testing for the feedstock token for" in str(e.value)
 
     assert git_mock.Repo.clone_from.called_with(
-        "abc123",
-        str(tmpdir),
-        depth=1,
+        "abc123", str(tmpdir), depth=1,
     )
 
 
-@pytest.mark.parametrize("repo", [
-    "GITHUB_TOKEN",
-    "${GITHUB_TOKEN}",
-    "GH_TOKEN",
-    "${GH_TOKEN}",
-])
+@pytest.mark.parametrize(
+    "repo", ["GITHUB_TOKEN", "${GITHUB_TOKEN}", "GH_TOKEN", "${GH_TOKEN}",]
+)
 @mock.patch("conda_smithy.feedstock_tokens.secrets")
 @mock.patch("conda_smithy.feedstock_tokens.os.urandom")
 @mock.patch("conda_smithy.feedstock_tokens.tempfile")
@@ -150,7 +136,9 @@ def test_register_feedstock_token_works(
     gh_mock, git_mock, tmp_mock, osuran_mock, secrets_mock, tmpdir, repo
 ):
     gh_mock.return_value = "abc123"
-    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(tmpdir)
+    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(
+        tmpdir
+    )
     secrets_mock.token_hex.return_value = "fgh"
     osuran_mock.return_value = b"\x80SA"
 
@@ -170,14 +158,14 @@ def test_register_feedstock_token_works(
             os.remove(pth)
 
     assert git_mock.Repo.clone_from.called_with(
-        "abc123",
-        str(tmpdir),
-        depth=1,
+        "abc123", str(tmpdir), depth=1,
     )
 
     repo = git_mock.Repo.clone_from.return_value
     assert repo.index.add.called_with(token_json_pth)
-    assert repo.index.commit.called_with("added token for %s/%s" % (user, project))
+    assert repo.index.commit.called_with(
+        "added token for %s/%s" % (user, project)
+    )
     assert repo.remote.return_value.pull.called_with(rebase=True)
     assert repo.remote.return_value.push.called_with()
 
@@ -191,12 +179,9 @@ def test_register_feedstock_token_works(
         assert json.load(fp) == data
 
 
-@pytest.mark.parametrize("repo", [
-    "GITHUB_TOKEN",
-    "${GITHUB_TOKEN}",
-    "GH_TOKEN",
-    "${GH_TOKEN}",
-])
+@pytest.mark.parametrize(
+    "repo", ["GITHUB_TOKEN", "${GITHUB_TOKEN}", "GH_TOKEN", "${GH_TOKEN}",]
+)
 @mock.patch("conda_smithy.feedstock_tokens.secrets")
 @mock.patch("conda_smithy.feedstock_tokens.os.urandom")
 @mock.patch("conda_smithy.feedstock_tokens.tempfile")
@@ -206,7 +191,9 @@ def test_register_feedstock_token_notoken(
     gh_mock, git_mock, tmp_mock, osuran_mock, secrets_mock, tmpdir, repo
 ):
     gh_mock.return_value = "abc123"
-    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(tmpdir)
+    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(
+        tmpdir
+    )
     secrets_mock.token_hex.return_value = "fgh"
     osuran_mock.return_value = b"\x80SA"
 
@@ -236,12 +223,9 @@ def test_register_feedstock_token_notoken(
     assert "No token found in" in str(e.value)
 
 
-@pytest.mark.parametrize("repo", [
-    "GITHUB_TOKEN",
-    "${GITHUB_TOKEN}",
-    "GH_TOKEN",
-    "${GH_TOKEN}",
-])
+@pytest.mark.parametrize(
+    "repo", ["GITHUB_TOKEN", "${GITHUB_TOKEN}", "GH_TOKEN", "${GH_TOKEN}",]
+)
 @mock.patch("conda_smithy.feedstock_tokens.secrets")
 @mock.patch("conda_smithy.feedstock_tokens.os.urandom")
 @mock.patch("conda_smithy.feedstock_tokens.tempfile")
@@ -251,7 +235,9 @@ def test_register_feedstock_token_exists_already(
     gh_mock, git_mock, tmp_mock, osuran_mock, secrets_mock, tmpdir, repo
 ):
     gh_mock.return_value = "abc123"
-    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(tmpdir)
+    tmp_mock.TemporaryDirectory.return_value.__enter__.return_value = str(
+        tmpdir
+    )
     secrets_mock.token_hex.return_value = "fgh"
     osuran_mock.return_value = b"\x80SA"
 
@@ -274,9 +260,7 @@ def test_register_feedstock_token_exists_already(
             os.remove(pth)
 
     assert git_mock.Repo.clone_from.called_with(
-        "abc123",
-        str(tmpdir),
-        depth=1,
+        "abc123", str(tmpdir), depth=1,
     )
 
     repo = git_mock.Repo.clone_from.return_value
