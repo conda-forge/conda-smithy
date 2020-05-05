201ee73be87a8a47366c5a1a58b886c13b99b8bd
Chris Burr <chrisburr@users.noreply.github.com>
2020-05-05 22:12:17 +0200

    Always require a licence file

--- conda_smithy/lint_recipe.py
+++ conda_smithy/lint_recipe.py
@@ -290,10 +290,8 @@ def lintify(meta, recipe_dir=None, conda_forge=False):
 
     # 12a: License family must be valid (conda-build checks for that)
     license_family = about_section.get("license_family", license).lower()
-    license_file = about_section.get("license_file", "")
-    if license_file == "" and any(
-        f for f in NEEDED_FAMILIES if f in license_family
-    ):
+    license_file = about_section.get("license_file", None)
+    if not license_file:
         lints.append("license_file entry is missing, but is required.")
 
     # 13: Check that the recipe name is valid
