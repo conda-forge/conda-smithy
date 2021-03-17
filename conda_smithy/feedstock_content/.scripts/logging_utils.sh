d929bb11bc055645677f343fc9863d736263d493
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-03-17 16:18:59 +0100

    temporarily disable set -x if it was set

--- conda_smithy/feedstock_content/.scripts/logging_utils.sh
+++ conda_smithy/feedstock_content/.scripts/logging_utils.sh
@@ -7,6 +7,15 @@
 function startgroup {
     # Start a foldable group of log lines
     # Pass a single argument, quoted
+
+    # Ccheck the status of xtrace
+    # We want to disable it if it's ON to
+    # prevent duplicated group tags
+    setx_was_on=false
+    if [ -o xtrace ]; then
+        set +x  # disable temporarily
+        setx_was_on=true
+    fi
     case ${CI:-} in
         azure )
             echo "##[group]$1";;
@@ -16,15 +25,26 @@ function startgroup {
         * )
             echo "$1";;
     esac
+
+    # If it was on, reenable
+    $setx_was_on && set -x
 }
 
 function endgroup {
     # End a foldable group of log lines
     # Pass a single argument, quoted
+
+    setx_was_on=false
+    if [ -o xtrace ]; then
+        set +x
+        setx_was_on=true
+    fi
     case ${CI:-} in
         azure )
             echo "##[endgroup]";;
         travis )
             echo -en 'travis_fold:end:'"${1// /}"'\\r';;
     esac
+
+    $setx_was_on && set -x
 }
