cdc9583545b5f988f6a2e02b48547377c6ada1cf
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-03-17 21:51:40 +0100

    support gha; sandbox silent xtrace and leave padding blank lines

--- conda_smithy/feedstock_content/.scripts/logging_utils.sh
+++ conda_smithy/feedstock_content/.scripts/logging_utils.sh
@@ -7,44 +7,29 @@
 function startgroup {
     # Start a foldable group of log lines
     # Pass a single argument, quoted
-
-    # Ccheck the status of xtrace
-    # We want to disable it if it's ON to
-    # prevent duplicated group tags
-    setx_was_on=false
-    if [ -o xtrace ]; then
-        set +x  # disable temporarily
-        setx_was_on=true
-    fi
     case ${CI:-} in
         azure )
             echo "##[group]$1";;
         travis )
             echo "$1"
             echo -en 'travis_fold:start:'"${1// /}"'\\r';;
+        github_actions )
+            echo "::group::$1";;
         * )
             echo "$1";;
     esac
-
-    # If it was on, reenable
-    $setx_was_on && set -x
-}
+} 2> /dev/null
 
 function endgroup {
     # End a foldable group of log lines
     # Pass a single argument, quoted
 
-    setx_was_on=false
-    if [ -o xtrace ]; then
-        set +x
-        setx_was_on=true
-    fi
     case ${CI:-} in
         azure )
             echo "##[endgroup]";;
         travis )
             echo -en 'travis_fold:end:'"${1// /}"'\\r';;
+        github_actions )
+            echo "::endgroup::";;
     esac
-
-    $setx_was_on && set -x
-}
+} 2> /dev/null
