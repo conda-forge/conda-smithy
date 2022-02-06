f54e2cc483c4ec32563e95af2ef07f4efe4f39ea
Uwe L. Korn <uwe.korn@quantco.com>
2022-02-06 10:03:11 +0100

    pre-commit run -a

--- extract_licenses.bash
+++ extract_licenses.bash
@@ -4,4 +4,3 @@
 # Capture first table
 # Extract code entries from table
 wget -q -O - https://spdx.org/licenses/ |  sed '/<table/,/<\/table>/!d;/<\/table>/q' | awk -- 'BEGIN{FS=">";RS="</code"};/<code/{print $NF}'
-
