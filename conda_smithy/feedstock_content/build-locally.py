ef31681a244699753a2eff3903d4f34555d2f23d
Isuru Fernando <isuruf@gmail.com>
2022-02-17 12:07:33 -0600

    Update conda_smithy/feedstock_content/build-locally.py

--- conda_smithy/feedstock_content/build-locally.py
+++ conda_smithy/feedstock_content/build-locally.py
@@ -61,7 +61,7 @@ def verify_config(ns):
         raise ValueError(
             f"only Linux/macOS configs currently supported, got {ns.config}"
         )
-    elif ns.config.startswith("osx") and platform.system() == "Darwin":
+    elif ns.config.startswith("osx"):
         if "OSX_SDK_DIR" not in os.environ:
             raise RuntimeError(
                 "Need OSX_SDK_DIR env variable set. Run 'export OSX_SDK_DIR=SDKs' "
