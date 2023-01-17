9d2982fe1cd61c5d7532bb199bcd2962c9048c85
beckermr <becker.mr@gmail.com>
2023-01-17 15:05:41 -0600

    REF move to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="conda-smithy",
-    description="A package to create repositories for conda recipes, and automate "
-    "their building with CI tools on Linux, OSX and Windows.",
-    author="Phil Elson",
-    author_email="pelson.pub@gmail.com",
-    url="https://github.com/conda-forge/conda-smithy",
-    entry_points=dict(
-        console_scripts=[
-            "feedstocks = conda_smithy.feedstocks:main",
-            "conda-smithy = conda_smithy.cli:main",
-        ]
-    ),
-    include_package_data=True,
-    packages=["conda_smithy"],
-    # As conda-smithy has resources as part of the codebase, it is
-    # not zip-safe.
-    zip_safe=False,
-    use_scm_version={
-        "write_to": "conda_smithy/_version.py",
-        "write_to_template": '__version__ = "{version}"',
-        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    },
-)
+setup()
