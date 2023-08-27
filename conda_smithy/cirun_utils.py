37d0252b0aa4ef96133ff52c7093466560666faa
Amit Kumar <dtu.amit@gmail.com>
2023-08-27 13:10:16 +0530

    add some more logging

--- conda_smithy/cirun_utils.py
+++ conda_smithy/cirun_utils.py
@@ -34,13 +34,14 @@ def add_repo_to_cirun_resource(owner, repo, resource, cirun_policy_args):
     if cirun_policy_args:
         if "pull_request" in cirun_policy_args:
             policy_args["pull_request"] = True
-
+    print(f"Adding repo {owner}/{repo} to resource {resource} with policy_args: {policy_args}")
     cirun.add_repo_to_resources(owner, repo, resources=[resource], teams=[repo], policy_args=policy_args)
 
 
 def remove_repo_from_cirun_resource(owner, repo, resource):
     """Revoke access to a cirun resource to a particular repository, with a particular policy."""
     cirun = _get_cirun_client()
+    print(f"Removing repo {owner}/{repo} from resource {resource}.")
     cirun.remove_repo_from_resources(owner, repo, [resource])
 
 
