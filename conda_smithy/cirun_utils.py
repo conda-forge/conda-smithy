6355aa803349bde0025ea1f295e24aba7c685e01
Amit Kumar <dtu.amit@gmail.com>
2023-08-27 13:08:13 +0530

    assert installation id

--- conda_smithy/cirun_utils.py
+++ conda_smithy/cirun_utils.py
@@ -9,6 +9,7 @@ def enable_cirun_for_project(owner, repo):
     """Enable the cirun.io Github Application for a particular repository."""
     print(f"Enabling cirun for {owner}/{repo} ...")
     cirun = _get_cirun_client()
+    assert CIRUN_INSTALLATION_ID
     return cirun.set_repo(
         f"{owner}/{repo}", installation_id=CIRUN_INSTALLATION_ID
     )
