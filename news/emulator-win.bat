8fc74a12137a0d8a4c20bb0c166811baca129131
Isuru Fernando <isuruf@gmail.com>
2023-11-26 14:40:53 -0600

    Don't skip tests if there is an emulator

---
+++ news/emulator-win.bat
@@ -0,0 +1,23 @@
+**Added:**
+
+* Don't skip testing in win if there is an emulator.
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* <news item>
+
+**Security:**
+
+* <news item>
