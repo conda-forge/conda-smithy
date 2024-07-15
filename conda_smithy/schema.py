c0c226929a0cf3875b1db4de1c3badbf14893f7a
jaimergp <jaimergp@users.noreply.github.com>
2024-07-15 20:40:36 +0200

    clarify comment

--- conda_smithy/schema.py
+++ conda_smithy/schema.py
@@ -42,7 +42,7 @@ conda_build_tools = Literal[
     "conda-build+conda-libmamba-solver",
     # will run 'conda mambabuild', as provided by boa
     "mambabuild",
-    # will run 'rattler-build',
+    # will run 'rattler-build build'
     "rattler-build",
 ]
 
