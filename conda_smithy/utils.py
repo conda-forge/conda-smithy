4cb789bf418b4086ea25e74ee1b10d29e66fac0e
nichmor <nmorkotilo@gmail.com>
2024-07-30 18:27:00 +0300

    Update conda_smithy/utils.py
    Co-authored-by: Bas Zalmstra <zalmstra.bas@gmail.com>

--- conda_smithy/utils.py
+++ conda_smithy/utils.py
@@ -319,7 +319,7 @@ def _get_metadata_from_feedstock_dir(
 def get_feedstock_name_from_metadata(
     meta: Union[MetaData, RattlerBuildMetaData],
 ) -> str:
-    """Resolve the feedtstock name from the parsed meta.yaml."""
+    """Get the feedstock name from a parsed meta.yaml or recipe.yaml."""
     return get_feedstock_name_from_meta(meta)
 
 
