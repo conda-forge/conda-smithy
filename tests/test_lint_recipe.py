0481ec62e1d1220a0e703dbd7a0a8d64da086984
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-08-09 23:03:13 +0200

    remove print

--- tests/test_lint_recipe.py
+++ tests/test_lint_recipe.py
@@ -2692,7 +2692,6 @@ def test_v1_recipes():
 def test_v1_no_test():
     with get_recipe_in_dir("v1_recipes/recipe-no-tests.yaml") as recipe_dir:
         lints, hints = linter.main(str(recipe_dir), return_hints=True)
-        print(lints)
         assert "The recipe must have some tests." in lints
 
 
