aa35321ec79d1a1f00ae202c11a79cdbf4ae633c
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-08-30 10:37:50 -0500

    fix: exclude pdm-backend from pip build backend lint

--- conda_smithy/linter/hints.py
+++ conda_smithy/linter/hints.py
@@ -181,6 +181,9 @@ def hint_check_spdx(about_section, hints):
 
 
 def hint_pip_no_build_backend(host_or_build_section, package_name, hints):
+    if package_name in ["pdm-backend"]:
+        return
+
     if host_or_build_section and any(
         req.split(" ")[0] == "pip" for req in host_or_build_section
     ):
