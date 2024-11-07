e0187cacdc550279ed79d18b30c1cce231e373d5
Hadrien Mary <hadim@users.noreply.github.com>
2024-11-07 15:02:27 -0500

    Update conda_smithy/feedstock_content/build-locally.py
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- conda_smithy/feedstock_content/build-locally.py
+++ conda_smithy/feedstock_content/build-locally.py
@@ -29,7 +29,8 @@ def setup_environment(ns):
     # The default cache location might not be writable using docker on macOS.
     if ns.config.startswith("linux") and platform.system() == "Darwin":
         os.environ["CONDA_FORGE_DOCKER_RUN_ARGS"] = (
-            "-e XDG_CACHE_HOME=/tmp/rattler_cache"
+            os.environ.get("CONDA_FORGE_DOCKER_RUN_ARGS", "")
+            + " -e RATTLER_CACHE_DIR=/tmp/rattler_cache"
         )
 
 
