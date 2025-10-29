23e4895eddc5dfa9782c4bca12b86e74f8df6933
jaimergp <jaimergp@users.noreply.github.com>
2025-10-29 02:14:05 +0100

    deprecate errors.py

--- conda_smithy/linter/errors.py
+++ conda_smithy/linter/errors.py
@@ -1,6 +1,7 @@
 """
-Collection of errors that can be raised by the linter.
+DEPRECATED. Moved to messages.py.
 """
 
-HINT_NO_ARCH = """Whenever possible python packages should use noarch.
-                See https://conda-forge.org/docs/maintainer/knowledge_base.html#noarch-builds"""
+from conda_smithy.linter.messages import RecipeSuggestNoarch
+
+HINT_NO_ARCH = RecipeSuggestNoarch.message  # DEPRECATED
