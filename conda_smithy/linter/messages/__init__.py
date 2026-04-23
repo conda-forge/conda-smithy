5262f51f45572d67503c2380312d319a8b5c5260
jaimergp <jaimergp@users.noreply.github.com>
2026-04-23 13:03:58 +0200

    Clarify docstring

--- conda_smithy/linter/messages/__init__.py
+++ conda_smithy/linter/messages/__init__.py
@@ -1,7 +1,7 @@
 """
 This subpackage contains all linter messages. To add new linter rules,
 find the right category in the accompanying modules and add a new dataclass
-definition. Identifier must be unique across the whole subpackage.
+definition. Identifier must be unique across all modules in this subpackage.
 
 Once ready, regenerate the docs with `python -m conda_smithy.linter.messages`.
 """
