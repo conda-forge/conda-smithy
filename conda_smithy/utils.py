9f40867fea8ac1e534500aeb7b7e1f641044dcab
Michał Górny <mgorny@quansight.com>
2026-06-13 14:13:03 +0200

    Raise an error if resize_partitions is enabled on non-GHA/win
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- conda_smithy/utils.py
+++ conda_smithy/utils.py
@@ -372,6 +372,13 @@ def get_workflow_settings(
                 f"workflows: {win_path}"
             )
 
+    if data["resize_partitions"]:
+        if os != "win" or provider != "github_actions":
+            raise ValueError(
+                "`workflow_settings.resize_partitions` is only valid for GHA/Windows "
+                f"(enabled for {provider=} / {os=})"
+            )
+
     return data
 
 
