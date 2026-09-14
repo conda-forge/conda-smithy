0afb30579137dd0e20b60f61dd6eb0013ccaea3f
beckermr <becker.mr@gmail.com>
2026-09-14 11:59:39 -0500

    fix: do not need string formatting here

--- conda_smithy/linter/messages/conda_forge.py
+++ conda_smithy/linter/messages/conda_forge.py
@@ -285,5 +285,3 @@ class UploadOnAllBranches(LinterMessage):
     identifier = "CF-014"
     added_in = "2026.9"
     message = "The feedstock is currently configured to upload on all branches. Please specify the branches for which you'd like package uploads in the `upload_branches` option in the `conda-forge.yml`. This hint will become a lint on or after `conda-smithy` version `2026.9.15`.\n"
-    current: str
-    expected: str
